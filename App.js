7a7d2ca85b73ae7e651e1af4b0cc727859e97ad5
Fendy <76032813+fenry46@users.noreply.github.com>
2023-03-09 18:34:02 +0700

    Apk & axios
    Build to apk and put axios code inside

--- App.js
+++ App.js
@@ -1,33 +1,67 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SafeAreaView } from "react-native";
-import IndexHomeScreen from './src/components/screens/IndexHome';
-import WelcomeScreen from './src/components/screens/Welcome';
-import globalStyles from './src/globalStyles';
-import LoginScreen from './src/components/screens/LoginScreen';
-import FirstPageRegister from './src/components/screens/FirstPageRegister';
-import SecondPageRegister from './src/components/screens/SecondPageRegister';
-import ResetPasswordSuccess from './src/components/screens/ResetPasswordSuccess';
-import ResetPasswordFailed from './src/components/screens/ResetPasswordFailed';
-import ForgotPassword from './src/components/screens/ForgotPassword';
+import IndexHomeScreen from "./src/components/screens/IndexHome";
+import WelcomeScreen from "./src/components/screens/Welcome";
+import globalStyles from "./src/globalStyles";
+import LoginScreen from "./src/components/screens/LoginScreen";
+import FirstPageRegister from "./src/components/screens/FirstPageRegister";
+import SecondPageRegister from "./src/components/screens/SecondPageRegister";
+import ResetPasswordSuccess from "./src/components/screens/ResetPasswordSuccess";
+import ResetPasswordFailed from "./src/components/screens/ResetPasswordFailed";
+import ForgotPassword from "./src/components/screens/ForgotPassword";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function App() {
-  const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator();
+
+  const getData = async () => {
+    try {
+      const res = await axios.get("", {
+        params: {},
+        headers: {},
+      });
+
+      console.log(res);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <SafeAreaView style={globalStyles.droidSafeArea}>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerShown: false,
-        }} initialRouteName="Home">
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+          initialRouteName="Home"
+        >
           <Stack.Screen name="BottomTab" component={IndexHomeScreen} />
           <Stack.Screen name="Home" component={WelcomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="FirstPageRegister" component={FirstPageRegister} />
-          <Stack.Screen name="SecondPageRegister" component={SecondPageRegister} />
-          <Stack.Screen name='ResetPasswordSuccess' component={ResetPasswordSuccess} />
-          <Stack.Screen name='ResetPasswordFailed' component={ResetPasswordFailed} />
-          <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
+          <Stack.Screen
+            name="FirstPageRegister"
+            component={FirstPageRegister}
+          />
+          <Stack.Screen
+            name="SecondPageRegister"
+            component={SecondPageRegister}
+          />
+          <Stack.Screen
+            name="ResetPasswordSuccess"
+            component={ResetPasswordSuccess}
+          />
+          <Stack.Screen
+            name="ResetPasswordFailed"
+            component={ResetPasswordFailed}
+          />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
